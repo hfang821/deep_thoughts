714847a130683faeaa4fbf30bc2393594090d747
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-07-14 16:40:43 -0400

    add functionality of add thoughts, add reactions and add friends and their corresponding authenitications

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const {User, Thought} = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const {signToken} = require('../utils/auth'); 
 
 const resolvers = {
     Query: {
@@ -25,14 +26,28 @@ const resolvers = {
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        },
+
+        //Ask Ta: what is context?
+        me: async(parent,args, context) => {
+        if(context.user) {
+            const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
+                
+            return userData;
+        }
+
+        throw new AuthenticationError('Not logged in');         
         }
     },
 
     Mutation: {
         addUser: async(parent, args) => {
             const user = await User.create(args);
-
-            return user;
+            const token = signToken(user);
+            return {token, user};
         },
 
         login: async(parent, {email, password}) => {
@@ -48,7 +63,52 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect Credentials');
             }
 
-            return user;
+            const token = signToken(user);
+            return {token,user};
+        },
+
+        addThought: async(parent, args, context) => {
+            if(context.user) {
+                const thought = await Thought.create({...args, username: context.user.username});
+
+                await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push: {thoughts: thought._id}},
+                    {new: true}
+                );
+            
+                return thought;
+            }
+
+            throw new AuthenticationError('You need to be logged in');
+        },
+
+        addReaction: async(parent, {thoughtId, reactionBody}, context) => {
+            if(context.user) {
+                const updatedThought = await Thought.findOneAndUpdate(
+                    {_id: thoughtId},
+                    {$push: {reactions: {reactionBody, username: context.user.username}}},
+                    {new: true, runValidators: true}
+                );
+
+                return updatedThought;
+            }
+
+            throw new AuthenticationError('You need to be logged in');
+        },
+
+        addFriend: async(parent, {friendId}, context) => {
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {friends: friendId}},
+                    {new: true}
+                ).populate('friends');
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in');
         }
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {authMiddleware} = require('./utils/auth');
 //import ApolloServer
 const {ApolloServer} = require('apollo-server-express');
 
@@ -10,8 +11,9 @@ const PORT = process.env.PORT || 3001;
 //create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs, 
-  resolvers
-})
+  resolvers,
+  context: authMiddleware
+});
 
 const app = express();
 
