80416eb05750a53ccb2e9e0ece3596875ce2cb64
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-07-13 21:32:57 -0400

    create add user and login mutations;

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const {User, Thought} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -16,7 +17,7 @@ const resolvers = {
             return User.find()
                 .select('-__v -password')
                 .populate('friends')
-                .populate('thoughts');
+                .populate('thoughts'); 
         },
 
         user: async(parent, {username}) => {
@@ -24,6 +25,30 @@ const resolvers = {
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        }
+    },
+
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+
+            return user;
+        },
+
+        login: async(parent, {email, password}) => {
+            const user = await User.findOne({email});
+            
+            if(!user){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            return user;
         }
     }
 };
